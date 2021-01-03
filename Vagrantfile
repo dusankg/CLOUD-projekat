ed030dc66d7f7e219fb4c462a4987f0a8dc2630c
dusankg <dusan1998kg@gmail.com>
2021-01-03 23:55:09 +0100

    Dodat docker-compose u vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -2,6 +2,16 @@
 Vagrant.configure("2") do |config|
 
   config.vm.box = "hashicorp/bionic64"
+
+
+  config.vm.provision "file", source: "nginx", destination: "nginx"
+  config.vm.provision "file", source: "Cloud", destination: "Cloud"
+
+  config.vm.provision "file", source: "Dockerfile", destination: "Dockerfile"
+  config.vm.provision "file", source: "docker-compose.yml", destination: "docker-compose.yml"
+  config.vm.network "forwarded_port", guest: 8083, host: 8083
+
+  
   config.vm.provision :shell, path: "script.sh"
 
 end
--- script.sh
+++ script.sh
@@ -16,8 +16,18 @@
 #sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 # DRUGI NACIN INSTALACIJE DOKERA
-curl -fsSL https://get.docker.com -o get-docker.sh
+curl -fsSL "https://get.docker.com" -o get-docker.sh
 sudo sh get-docker.sh
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
+sudo apt-get update
+sudo apt-get install nginx -y
+
+sudo service nginx stop
+
+#cd Cloud
+docker-compose build
+docker-compose up
 
